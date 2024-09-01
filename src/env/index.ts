7411532a9a165ab221f370fbc10db9ad20e8ca90
Matheus Lucas <m7hsouza@gmail.com>
2024-09-01 12:23:18 -0400

    chore: Update dotenv configuration and add .env.test file
    This commit updates the dotenv configuration in the `env/index.ts` file. It replaces the import statement for `dotenv/config` with `dotenv/config` to ensure that the environment variables are loaded correctly. Additionally, it adds a new conditional block to load the `.env.test` file when the `NODE_ENV` is set to 'test'. This change improves the configuration setup and allows for separate environment variables during testing.

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,10 +1,16 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  DATABASE_URL: z.string()
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
