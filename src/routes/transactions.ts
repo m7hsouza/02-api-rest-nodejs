937e1bcf00481698690856f2aaed7733eb99d2ec
Matheus Lucas <m7hsouza@gmail.com>
2024-09-01 08:27:31 -0400

    feat: Add summary route for retrieving transaction summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,14 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance): Promise<void> {
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*')
 
